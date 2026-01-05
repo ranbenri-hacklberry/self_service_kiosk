13a9eecad3cb132dc2f077aaa323426d6f883816
ranbenri-hacklberry <ranbenri@gmail.com>
2026-01-05 06:32:48 +0200

    Bump version to 3.1.0

--- src/config/version.js
+++ src/config/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = 'β 0.10.0';
+export const APP_VERSION = '3.1.0';
 export const VERSION_DATE = '2026-01-05';
-export const VERSION_NOTES = 'Smart Kg display & snap-to-grid counting in inventory';
+export const VERSION_NOTES = 'Item location display in KDS inventory, smart Kg display & snap-to-grid counting';
